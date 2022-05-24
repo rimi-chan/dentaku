c330a75de8c92fc6085ef091e221b981e8a25d15
EC2 Default User <ec2-user@ip-172-31-42-21.ap-northeast-1.compute.internal>
2022-05-24 07:23:50 +0000

    小数点、演算子の連続入力阻止

--- dentaku.js
+++ dentaku.js
@@ -1,17 +1,14 @@
 let is_calc = false;
 // // =で計算したかどうか
+let dec = false;
+// 小数点使用済み用
 let result = document.getElementById("result");
-
-//ACボタンの呼び出し
-// function c_click() {
-//     result.value = "0";
-// //     //リセットなので＝で計算してない
-//     is_calc = false;
-// }
+let botton = document.getElementById("botton");
 
 function ac_click(){
   result.value = "0";
   is_calc = false;
+//   dec = false;
 }
 
 function num_click(val){
@@ -24,17 +21,29 @@ function num_click(val){
 //     //0を入力して.を入力
     if(result.value == "0" && val == "0"){
         result.value = "0";
-    }else if(result.value == "0" && val == "."){
-        result.value = "0.";
+        dec = false;
     }else if(result.value == "0"){
         result.value = val;
+        dec = false;
     }else{
         result.value += val;
+        dec = false;
         // 二桁表示にするための設定
         // += 加算もしくは連結が可能/
     }
     
 }
+
+
+
+function dotto_click(val){
+    if(!dec){
+        result.value = result.value + val;
+        dec = true;
+    }
+}
+
+
 
 
 function ope_click(val){
@@ -46,26 +55,30 @@ function ope_click(val){
         // slice(0,-1) + val;最後は演算子で必ず終わるため＋⇨ーが可能になる。
     }else{
         result.value += val;
+        dec = false;
     }
 }
 
 
 function equal_click(){
     if(is_ope_last()) result.value = result.value.slice(0,-1);
+    // 計算列の最後が["+","-","*","/"]いずれかだったら["+","-","*","/"]を取り除く
     var temp = new Function("return " + result.value.replaceAll("x", "*").replaceAll("÷", "/"))();
-
+// 　　計算結果の設定
     if(temp == Infinity || Number.isNaN(temp)){
         result.value = "Error";
         
     }else{
         result.value = temp;
         is_calc = true;
+        dec = false;
+        
   }
 }
 
 
 function is_ope_last(){
-  return ["+","-","x","÷"].includes(result.value.toString().slice(-1));
+  return ["+","-","*","/"].includes(result.value.toString().slice(-1));
 }
-// result.value最後の文字(-1)に["+","-","×","÷"]いずれかがある
-// 文字列.includes(検索したい文字列)
+//["+","-","×","÷"]と一致するか
+// result.value.toString().slice(-1)」は計算式の最後の文字を取得
